ea35e2cbab574a3c869fb84e732dc347339b70cd
redthing1 <redthing1@alt.icu>
2023-05-15 17:07:55 -0700

    refactor wren list wrangling

--- src/source/wox/foreign/bind/w.d
+++ src/source/wox/foreign/bind/w.d
@@ -49,15 +49,7 @@ struct BindForeignW {
     struct W {
         // cliopts() -> list[string]
         static void cliopts(WrenVM* vm) {
-            // put all the cli args into a new list
-            wrenSetSlotNewList(vm, 0);
-            wrenEnsureSlots(vm, cast(int)(1 + wox_context.args.length));
-
-            foreach (i, arg; wox_context.args) {
-                auto el_ix = cast(int)(i + 1);
-                wrenSetSlotString(vm, el_ix, arg.toStringz);
-                wrenInsertInList(vm, 0, cast(int) i, el_ix);
-            }
+            Utils.wren_write_string_list(vm, 0, wox_context.args, 1);
         }
 
         // cliopt(name, default) -> string
@@ -106,109 +98,61 @@ struct BindForeignW {
             auto regex_str = pattern_str.replace("*", ".*");
             auto matching_files = Utils.recursive_listdir_matching(".", regex_str);
 
-            // put all the matching files into a new list
-            wrenSetSlotNewList(vm, 0);
-            wrenEnsureSlots(vm, cast(int)(1 + matching_files.length));
-
-            foreach (i, file; matching_files) {
-                auto el_ix = cast(int)(i + 1);
-                wrenSetSlotString(vm, el_ix, file.toStringz);
-                wrenInsertInList(vm, 0, cast(int) i, el_ix);
-            }
+            Utils.wren_write_string_list(vm, 0, matching_files, 1);
         }
 
         // ext_add(paths: list, ext) -> list[string]
         static void ext_add(WrenVM* vm) {
-            auto paths_len = wrenGetListCount(vm, 1);
+            auto paths = Utils.wren_read_string_list(vm, 1, 0);
             auto ext = wrenGetSlotString(vm, 2).to!string;
 
-            string[] results;
-            for (auto i = 0; i < paths_len; i++) {
-                // grab elements and put them in slot 0 temporarily
-                wrenGetListElement(vm, 1, i, 0);
-                auto path = wrenGetSlotString(vm, 0).to!string;
-                results ~= path ~ ext;
+            foreach (i, path; paths) {
+                // append extension to each path
+                paths[i] = path ~ ext;
             }
 
-            // put all the results into a new list
-            wrenSetSlotNewList(vm, 0);
-            wrenEnsureSlots(vm, cast(int)(1 + results.length));
-
-            foreach (i, result; results) {
-                auto el_ix = cast(int)(i + 1);
-                wrenSetSlotString(vm, el_ix, result.toStringz);
-                wrenInsertInList(vm, 0, cast(int) i, el_ix);
-            }
+            Utils.wren_write_string_list(vm, 0, paths, 1);
         }
 
         // ext_replace(paths: list, ext, new_ext) -> list[string]
         static void ext_replace(WrenVM* vm) {
-            auto paths_len = wrenGetListCount(vm, 1);
+            auto paths = Utils.wren_read_string_list(vm, 1, 0);
             auto ext = wrenGetSlotString(vm, 2).to!string;
             auto new_ext = wrenGetSlotString(vm, 3).to!string;
 
-            string[] results;
-            for (auto i = 0; i < paths_len; i++) {
-                // grab elements and put them in slot 0 temporarily
-                wrenGetListElement(vm, 1, i, 0);
-                auto path = wrenGetSlotString(vm, 0).to!string;
-                results ~= path.replace(ext, new_ext);
+            foreach (i, path; paths) {
+                // replace extension in each path
+                paths[i] = path.replace(ext, new_ext);
             }
 
-            // put all the results into a new list
-            wrenSetSlotNewList(vm, 0);
-            wrenEnsureSlots(vm, cast(int)(1 + results.length));
-
-            foreach (i, result; results) {
-                auto el_ix = cast(int)(i + 1);
-                wrenSetSlotString(vm, el_ix, result.toStringz);
-                wrenInsertInList(vm, 0, cast(int) i, el_ix);
-            }
+            Utils.wren_write_string_list(vm, 0, paths, 1);
         }
 
         // ext_remove(paths: list, ext) -> list[string]
         static void ext_remove(WrenVM* vm) {
-            auto paths_len = wrenGetListCount(vm, 1);
+            auto paths = Utils.wren_read_string_list(vm, 1, 0);
             auto ext = wrenGetSlotString(vm, 2).to!string;
 
-            string[] results;
-            for (auto i = 0; i < paths_len; i++) {
-                // grab elements and put them in slot 0 temporarily
-                wrenGetListElement(vm, 1, i, 0);
-                auto path = wrenGetSlotString(vm, 0).to!string;
+            foreach (i, path; paths) {
+                // remove extension from each path
                 if (ext == "") {
                     // if ext is empty, remove the last extension
                     auto last_dot_ix = path.lastIndexOf(".");
-                    results ~= path[0 .. last_dot_ix];
+                    paths[i] = path[0 .. last_dot_ix];
                 } else {
                     // otherwise, remove the specified extension
-                    results ~= path.replace(ext, "");
+                    paths[i] = path.replace(ext, "");
                 }
             }
 
-            // put all the results into a new list
-            wrenSetSlotNewList(vm, 0);
-            wrenEnsureSlots(vm, cast(int)(1 + results.length));
-
-            foreach (i, result; results) {
-                auto el_ix = cast(int)(i + 1);
-                wrenSetSlotString(vm, el_ix, result.toStringz);
-                wrenInsertInList(vm, 0, cast(int) i, el_ix);
-            }
+            Utils.wren_write_string_list(vm, 0, paths, 1);
         }
 
         // path_join(paths: list) -> string
         static void path_join(WrenVM* vm) {
-            auto paths_len = wrenGetListCount(vm, 1);
+            auto paths = Utils.wren_read_string_list(vm, 1, 0);
 
-            string[] path_segments;
-            for (auto i = 0; i < paths_len; i++) {
-                // grab elements and put them in slot 0 temporarily
-                wrenGetListElement(vm, 1, i, 0);
-                auto path = wrenGetSlotString(vm, 0).to!string;
-                path_segments ~= path;
-            }
-            string joined_path = std.path.buildPath(path_segments);
+            auto joined_path = std.path.buildPath(paths);
 
             wrenSetSlotString(vm, 0, joined_path.toStringz);
         }
@@ -218,14 +162,8 @@ struct BindForeignW {
             auto path = wrenGetSlotString(vm, 1).to!string;
 
             auto path_segments = path.split(std.path.dirSeparator);
-            // put all the path segments into a new list
-            wrenSetSlotNewList(vm, 0);
-            wrenEnsureSlots(vm, cast(int)(1 + path_segments.length));
-            foreach (i, path_segment; path_segments) {
-                auto el_ix = cast(int)(i + 1);
-                wrenSetSlotString(vm, el_ix, path_segment.toStringz);
-                wrenInsertInList(vm, 0, cast(int) i, el_ix);
-            }
+
+            Utils.wren_write_string_list(vm, 0, path_segments, 1);
         }
 
         // path_dirname(path) -> string
--- src/source/wox/foreign/utils.d
+++ src/source/wox/foreign/utils.d
@@ -37,4 +37,28 @@ static struct Utils {
 
         return matched_files;
     }
+
+    static string[] wren_read_string_list(WrenVM* vm, int list_slot, int temp_slot) {
+        auto str_list_len = wrenGetListCount(vm, 1);
+        string[] str_list_items;
+        for (auto i = 0; i < str_list_len; i++) {
+            wrenGetListElement(vm, 1, i, temp_slot);
+            str_list_items ~= wrenGetSlotString(vm, 0).to!string;
+        }
+
+        return str_list_items;
+    }
+
+    static void wren_write_string_list(WrenVM* vm, int list_slot, string[] str_list_items, int temp_slot) {
+        wrenEnsureSlots(vm, temp_slot + 1);
+        // create a new list in the destination slot
+        wrenSetSlotNewList(vm, list_slot);
+
+        for (auto i = 0; i < str_list_items.length; i++) {
+            // put the item in a slot
+            wrenSetSlotString(vm, temp_slot, str_list_items[i].toStringz);
+            // add the item to the list
+            wrenInsertInList(vm, list_slot, i, temp_slot);
+        }
+    }
 }
